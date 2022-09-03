5c6c4e9af67bdbc48467d5f1b773318449e48658
User <dg21stew@gmail.com>
2022-09-02 23:55:41 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList linkedList  = new LinkedList();
+
+        linkedList.add("Bob");
+        linkedList.add("John");
+        linkedList.add("Jim");
+        linkedList.add("Jack");
+        linkedList.add("Pam");
+        linkedList.add("Mary");
+
+        System.out.println(linkedList);
+
+
+    }
+
+
+}
